e63367d8e1f01be553a4d36b0bdaaf54fa7d911b
Cristian Sánchez <criiscz08@gmail.com>
2023-03-04 10:59:58 -0500

    feat(COM-55): started frontend dev

---
+++ frontend/src/app/pages/login/components/loginPage/login.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import './login.module.css';
+import {Box} from "@chakra-ui/react";
+
+const Login = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      width="100vw"
+      backgroundColor="yellow.500"
+    />
+  )
+};
+export default Login;
