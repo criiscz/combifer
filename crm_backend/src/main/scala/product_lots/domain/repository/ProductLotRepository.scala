5d2bdb3eed6d4986bc8e9776bcf0ff55965dbb13
Orlando Vargas <nojipiz@protonmail.com>
2023-03-13 21:50:15 -0500

    feature: base repository for product lots

--- crm_backend/src/main/scala/product_lots/domain/repository/ProductLotRepository.scala
+++ crm_backend/src/main/scala/product_lots/domain/repository/ProductLotRepository.scala
@@ -3,4 +3,11 @@ package product_lots.domain.repository
 import product_lots.domain.entity.ProductLot
 
 trait ProductLotRepository:
-  def getProduct(id: Long): Option[ProductLot]
+  def getLot(id: Long): Option[ProductLot]
+  def getLots(from:Int, to:Int): List[ProductLot]
+  def getLotsOfProduct(productId: Long, from:Int, to:Int): List[ProductLot]
+  def getTotalAmountOfLots():Long
+  def getTotalAmountOfLotsAtProduct(productId:Long):Long
+  def insertLot(lot:ProductLot): Unit
+  def updateLot(lot:ProductLot): Unit
+  def removeLot(id:Long): Unit
