206c4367ceb59d6bc86144006928bcd1b4029fda
Cristian Sánchez <criiscz08@gmail.com>
2023-03-18 19:54:49 -0500

    feat: remove

--- frontend/src/app/dashboard/inventory/components/ProductList/ProductList.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ProductList from './ProductList'
-
-describe('<ProductList />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ProductList products={[]}/>)
-  })
-})
