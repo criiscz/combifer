56bc83e2d73470ec49f0907dbed858f6d28ae4b5
Orlando Vargas <nojipiz@protonmail.com>
2023-04-01 09:37:28 -0500

    feature: Taxes CRUD

--- crm_backend/src/main/scala/taxes/infrastructure/controller/TaxController.scala
+++ crm_backend/src/main/scala/taxes/infrastructure/controller/TaxController.scala
@@ -16,6 +16,9 @@ import shared.mapper.endpoints.Exposer._
 import taxes.domain.entity.Tax
 import shared.responses._
 import taxes.application.get_taxes._
+import taxes.application.remove_tax._
+import taxes.application.create_tax._
+import taxes.application.update_tax._
 import taxes.domain.repository.TaxRepository
 import shared.BaseController
 
@@ -25,9 +28,8 @@ class TaxController()
   taxRepository:TaxRepository)
 extends BaseController:
 
-  private val getTaxes: ZPartialServerEndpoint[Any, AuthenticationToken, (UserContext, PermissionContext), (Int, Int), ApplicationError, PaginatedResponse[Tax], Any] = 
+  private val getTaxes =
     secureEndpoint
-    .get
     .in("taxes")
     .get
     .in(
@@ -45,3 +47,49 @@ extends BaseController:
       .mapError(e => ErrorResponse(message="Can't get taxes"))
     }.expose
 
+  private val removeTax = 
+    secureEndpoint
+    .in("taxes" / path[Long]("id"))
+    .delete
+    .errorOutVariant[ApplicationError](oneOfVariant(jsonBody[ErrorResponse]))
+    .out(jsonBody[ResponseRemoveTax])
+    .exposeSecure
+
+  private val removeTaxRoute: ZServerEndpoint[Any, Any] =
+    removeTax.serverLogic{ (user:UserContext, permission:PermissionContext) => (id:Long) =>
+      RemoveTaxUseCase().execute(
+          RequestRemoveTax(id)
+      ).mapError(e => ErrorResponse(message="Can't remove tax"))
+    }.expose
+
+  private val createTax = 
+    secureEndpoint
+    .in("taxes")
+    .in(jsonBody[RequestCreateTax])
+    .post
+    .errorOutVariant[ApplicationError](oneOfVariant(jsonBody[ErrorResponse]))
+    .out(jsonBody[ResponseCreateTax])
+    .exposeSecure
+
+  private val createTaxRoute: ZServerEndpoint[Any, Any] =
+    createTax.serverLogic{ (user:UserContext, permission:PermissionContext) => request =>
+      CreateTaxUseCase().execute(
+        request
+      ).mapError(e => ErrorResponse(message="Can't remove tax"))
+    }.expose
+
+  private val updateTax = 
+    secureEndpoint
+    .in("taxes" / path[Long]("id"))
+    .put
+    .in(jsonBody[RequestUpdateTax])
+    .errorOutVariant[ApplicationError](oneOfVariant(jsonBody[ErrorResponse]))
+    .out(jsonBody[ResponseUpdateTax])
+    .exposeSecure
+
+  private val updateTaxRoute: ZServerEndpoint[Any, Any] =
+    updateTax.serverLogic{ (user:UserContext, permission:PermissionContext) => (id: Long, request: RequestUpdateTax) =>
+      UpdateTaxUseCase(id).execute(
+        request
+      ).mapError(e => ErrorResponse(message="Can't update tax"))
+    }.expose
