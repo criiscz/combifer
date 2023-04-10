1c2cea9d0465620d9c944bb230c918b80ad435d0
Orlando Vargas <nojipiz@protonmail.com>
2023-04-09 23:42:26 -0500

    fix: Typo, imports and namesp

--- crm_backend/src/main/scala/Main.scala
+++ crm_backend/src/main/scala/Main.scala
@@ -26,6 +26,7 @@ import sttp.tapir.server.interceptor.cors.CORSInterceptor
 import taxes.infrastructure.controller.TaxController
 import sales.infrastructure.controller.SalesController
 import recommendations.infrastructure.controller.RecommendationController
+import agents.infrastructure.controller.ClientController
 
 
 object Main extends ZIOAppDefault with DI:
@@ -37,6 +38,7 @@ object Main extends ZIOAppDefault with DI:
   ProductLotController()
   TaxController()
   SalesController()
+  ClientController()
   RecommendationController()
 
   val serverOptions:ZioHttpServerOptions[Any] =
