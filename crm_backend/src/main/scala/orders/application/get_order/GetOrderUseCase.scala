f4d976397ebeee55b6a736295728352b38f56792
Orlando Vargas <nojipiz@protonmail.com>
2023-05-06 13:35:28 -0500

    Improved get order endpoint

--- crm_backend/src/main/scala/orders/application/get_order/GetOrderUseCase.scala
+++ crm_backend/src/main/scala/orders/application/get_order/GetOrderUseCase.scala
@@ -3,12 +3,24 @@ package orders.application.get_order
 import shared.application.BaseUseCase
 import zio._
 import orders.domain.repository.OrderRepository
+import order_products.domain.repository.OrderProductRepository
 
 class GetOrderUseCase
-(using orderRepository: OrderRepository)
+(using
+  orderRepository: OrderRepository,
+  orderProductRepository: OrderProductRepository
+)
 extends BaseUseCase[RequestGetOrder, ResponseGetOrder]:
 
   override def execute(request: RequestGetOrder): Task[ResponseGetOrder] =
-    orderRepository.getOrder(request.orderId) match
-      case None => ZIO.fail(new Throwable("Can't find order"))
-      case Some(value) => ZIO.succeed(ResponseGetOrder(value(0)))
+    for
+      orderInfo <- ZIO.fromOption(orderRepository.getOrder(request.orderId))
+        .mapError(e => Throwable("Can't find order info"))
+      orderProducts <- ZIO.succeed(orderProductRepository.getProductsOfOrderId(request.orderId))
+    yield (
+      ResponseGetOrder(
+        orderInfo(0),
+        orderInfo(1),
+        orderProducts
+      )
+    )
